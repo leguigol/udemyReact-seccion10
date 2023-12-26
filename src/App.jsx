135c60b2060e2b2bba876529c4c955499de3658a
legui <leguigol@gmail.com>
2023-12-25 22:47:57 -0300

    modificaciones del 25-12

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import Navbar from './components/Navbar'
+import {Routes,Route} from 'react-router-dom'
+
+import Inicio from './pages/inicio'
+import Contacto from './pages/Contacto'
+import Blog from './pages/Blog'
 
 const App=()=>{
   return (
   <>
     <Navbar />
     <h1>App</h1>
+    <Routes>
+      <Route element={<Inicio />} path="/"></Route>
+      <Route element={<Contacto />} path="/contacto"></Route>
+      <Route element={<Blog />} path="/blog"></Route>
+    </Routes>
   </>
   )
 }
